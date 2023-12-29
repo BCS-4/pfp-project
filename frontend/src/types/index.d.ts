e33bceedb7ca452f766f753dea9a8e398ceca6be
GRyu1 <heliotrop1024@gmail.com>
2023-12-29 16:25:10 +0900

    Initial commit

---
+++ frontend/src/types/index.d.ts
@@ -0,0 +1,27 @@
+import Web3, { Contract, ContractAbi } from "web3";
+
+export interface OutletContext {
+  account: string;
+  web3: Web3;
+  mintNftContract: Contract<ContractAbi>;
+  saleNftContract: Contract<ContractAbi>;
+}
+
+export interface NftMetadata {
+    tokenId?: number;
+    name: string;
+    image: string;
+    description: string;
+    attributes: {
+      trait_type: string;
+      value: string;
+    }[];
+  }
+
+  export interface NftCardProps {
+    image: string;
+    name: string;
+    tokenId: number;
+  }
+
+  
